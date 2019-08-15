11b7e4c40cfa5e074eb9755f7490e91ad3d66638
BL-CoderOvertime <brandonlively@live.com>
2019-08-15 09:49:49 -0400

    last pre-class update, maybe

--- app/src/main/java/com/atob/intentdemo/ui/BookListActivity.kt
+++ app/src/main/java/com/atob/intentdemo/ui/BookListActivity.kt
@@ -27,35 +27,7 @@ class BookListActivity : AppCompatActivity() {
             )
         }
     }
-
-    override fun onPostResume() {
-        refreshBookList()
-        super.onPostResume()
-    }
-
-    fun refreshBookList(){
-        ll_booklist.removeAllViews()
-        var counter = 0
-        for (book in bookList){
-            ll_booklist.addView(createTextView(book, counter))
-        }
-    }
-
-    fun createTextView(book:Book, index:Int):TextView{
-        var newBookView = TextView(this)
-        newBookView.textSize = 24F
-        newBookView.id = index
-        newBookView.text = book.title
-
-        newBookView.setOnClickListener {
-            var tvIntent = Intent(this, BookDetailsActivity::class.java)
-            tvIntent.putExtra("book", bookList[newBookView.id])
-            bookList.removeAt(newBookView.id)
-            startActivityForResult(tvIntent, REQUEST_CODE_EDIT_BOOK)
-        }
-        return newBookView
-    }
-
+    
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE_EDIT_BOOK && resultCode == RESULT_OK) {
             val newResultBook = data!!.getSerializableExtra("book") as Book
